7deff7463e46479d2489ff7cee7ce46dccf4ad5e
Joshua Moore <redir>
2021-09-22 15:55:19 -0600

    updates

---
+++ src/components/House.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="mb-4 listing">
+    <router-link :to="{name: 'House', params: { houseId: house.id }}">
+      <div class="card selectable">
+        <img :src="house.img" alt="listing image" class="rounded">
+        <div class="card-body">
+          <h5 class="d-flex justify-content-between">
+            <span>Bedrooms:{{ house.bedrooms }} Levels:{{ house.levels }} - Year Built:{{ house.year }} </span>
+            <span> ${{ house.price }} </span>
+          </h5>
+        </div>
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import { House } from '../models/House.js'
+import { logger } from '../utils/Logger.js'
+
+export default {
+  props: {
+    house: { type: House, required: true }
+  },
+  setup(props) {
+    return {
+      deleteHouse() {
+        logger.log('delete working?')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/models/House.js
+++ src/models/House.js
@@ -1,11 +1,14 @@
 export class House {
-  constructor(houseData) {
+  constructor(houseData = {}) {
     this.bedrooms = houseData.bedrooms
     this.year = houseData.year
     this.bathrooms = houseData.bathrooms
     this.levels = houseData.levels
-    this.imgUrl = houseData.imgUrl
+    this.img = houseData.img
     this.price = houseData.price
     this.description = houseData.description
+    this.creatorId = houseData.creatorId
+    this.creator = houseData.creator || {}
+    this.id = houseData.id
   }
 }
